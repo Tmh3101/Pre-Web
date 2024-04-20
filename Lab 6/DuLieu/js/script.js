4150826279a74dd6657d73de384df38d27fd214c
Tmh3101 <minhhieu31012004@gmail.com>
2024-04-20 23:40:13 +0700

    edit Lab 6

--- Lab 6/DuLieu/js/script.js
+++ Lab 6/DuLieu/js/script.js
@@ -4,14 +4,17 @@ function fromCheck(){
 
     var mssvValue = document.getElementById('mssv').value;
     var nameValue = document.getElementById('name').value;
+    var gioiTinhNam = document.getElementById('nam');
+    var gioiTinhNu = document.getElementById('nu');
     var dobValue = document.getElementById('dob').value;
+    var quequanValue = document.getElementById('quequan').value.trim();
     var avatarValue = document.getElementById('avatar').value;
-    var sdtValue = document.getElementById('sdt');
-    const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var emailValue = document.getElementById('email');
-    var passValue = document.getElementById('pss');
-    var pass2Value = document.getElementById('pss2');
-    
+    var sdtValue = document.getElementById('sdt').value;
+    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var emailValue = document.getElementById('email').value;
+    var passValue = document.getElementById('pss').value;
+    var pass2Value = document.getElementById('pss2').value;
+
     if(mssvValue == '') {
         check = false;
         alert("MSSV khong duoc de trong");
@@ -24,14 +27,18 @@ function fromCheck(){
         check = false;
         alert("Ho ten khong duoc de trong");
     }
+    else if(!gioiTinhNam.checked && !gioiTinhNu.checked){
+        check = false;
+        alert("Gioi tinh khong duoc de trong");
+    }
     else if(dobValue == ''){
         check = false;
         alert("Ngay sinh khong duoc de trong");
     }
-    // else if(document.getElementById('ht').value == null){
-    //     check = false;
-    //     alert("Que quan khong duoc de trong");
-    // }
+    else if(quequanValue == ''){
+        check = false;
+        alert("Que quan khong duoc de trong");
+    }
     else if(avatarValue == ''){
         check = false;
         alert("Anh dai dien khong duoc de trong");
@@ -55,10 +62,16 @@ function fromCheck(){
     return check;
 }
 
+document.getElementById('dk-btn').addEventListener('submit', function(event){
+    event.defaultPrevented();
+})
+
 document.getElementById('dk-btn').addEventListener('click', function(){
     if(fromCheck()){
         alert("Thanh cong");
         window.location = "index.html";
+    } else {
+        alert("That bai");
     }
 })
 
